a72e68edabda0470e8b5baa80a47ef956ab6095f
Woisol-G <Woisol-G@outlook.com>
2024-07-14 21:44:29 +0800

    feat(renamingWin): 初步完成设计

--- src/renderer/src/components/Home.tsx
+++ src/renderer/src/components/Home.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
-import { Check } from '@mui/icons-material';
+import { ArrowForward, Check } from '@mui/icons-material';
 import RecordsGroup from './HomeStream/RecordsGroup';
 import TitleBar from './TitleBar';
+import rightArrow from '../../../../resources/right-arrow.png'
 const emptySrc = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
 var originTarget: HTMLImageElement, originImgClientRects: ClientRect, isReachCenter: boolean = false;
 type Props = {
@@ -10,8 +11,9 @@ type Props = {
 }
 export default function Home({ curDir }: Props) {
 	const [displaySize, setDisplaySize] = useState(1)
-	const [detailWinOpen, setDetailWinOpen] = useState(false);
 	const [recordData, setRecordData] = useState([])
+	const [detailWinOpen, setDetailWinOpen] = useState(false);
+	const [renamineWinOpen, setRenamineWinOpen] = useState(false);
 	// @ts-ignore
 	useEffect(() => { window.store.get("displaySize").then(res => setDisplaySize(res)) }, [])
 
@@ -76,9 +78,47 @@ export default function Home({ curDir }: Props) {
 					// !woc！！！这！自己补全出来了tailwindcss的动画哈哈哈
 					:
 					<>
-						{recordData.map((recordData, index) => <RecordsGroup key={index} curDir={curDir} displaySize={displaySize} handleDetailWinOpen={handleDetailWinOpen} recordData={recordData} />)}
+						{recordData.map((recordData, index) => <RecordsGroup key={index} curDir={curDir} displaySize={displaySize} handleDetailWinOpen={handleDetailWinOpen} recordData={recordData} setRecordData={(value) => setRecordData(value)} />)}
 						<Button size='medium' variant='contained' style={{ width: '300px', marginLeft: 'auto', marginRight: 'auto' }} >加载更多</Button>
-						<button className='w-14 h-14 fixed top-14 right-10 z-30 rounded-full bg-blue-400 transition-all shadow-2xl hover:scale-110 active:bg-blue-500 active:scale-90 ring-0' title='Confirm'><Check style={{ width: '30px', height: '30px' }} /></button>
+						<div className={`${renamineWinOpen ? 'w-[400px] h-[600px] right-1/2 top-1/2 translate-x-1/2 -translate-y-1/2 ' : 'w-14 h-14 right-10 top-14 bg-white hover:scale-110 active:scale-90 '} fixed z-30 rounded-2xl shadow-2xl overflow-hidden transition-all duration-500 `}>
+							<div className={`${renamineWinOpen ? 'w-[400px]' : 'w-14'} h-14 absolute right-0 flex`}>
+								<button className={`w-[200px] h-14 rounded-l-2xl bg-gray-300 ${renamineWinOpen ? '' : 'hidden'}`} onClick={() => { setRenamineWinOpen(false) }}>取消</button>
+								<button className={`${renamineWinOpen ? 'w-[200px] duration-100 rounded-r-2xl ' : 'w-14 duration-500 rounded-2xl'} h-14 bg-blue-400 active:bg-blue-500 ring-0 transition-all `} title='Confirm' onClick={() => { setRenamineWinOpen(!renamineWinOpen) }}>
+									{renamineWinOpen ? '确认' : <Check style={{ width: '30px', height: '30px' }} />}
+								</button>
+							</div>
+							{renamineWinOpen &&
+								<>
+									<div className="w-full h-14"></div>
+									<div className="w-full h-[544px] p-5 bg-white text-xl">
+										<p className='my-2 mt-10'>选中数量：</p>
+										<p className='my-2'>最长间隔时间：</p>
+										{/* <div className='my-2'> */}
+										改名方案：
+										<input className='w-full mb-4 border-b-2 border-gray-500' title='renaming scheme' type="text" />
+										{/* </div> */}
+										{/* <div className='my-2'> */}
+										游戏名称{'{Game}'}：
+										{/* // !呼FT乱来，这样就可以显示{ }了 */}
+										<input className='w-full mb-4 border-b-2 border-gray-500' title='game name' type="text" />
+										{/* </div> */}
+										{/* <div className='my-2'> */}
+										自定义信息{'{Message}'}
+										<input className='w-full mb-4 border-b-2 border-gray-500' title='：' type="text" />
+										{/* </div> */}
+
+										<div className="w-full absolute bottom-10 text-center">
+											效果预览（以第一个文件为例）：
+											<div className="w-[360px] h-14 px-2 bg-gray-200 rounded-2xl flex justify-center items-center">
+												<span className='w-40 text-xs text-left'>{recordData[0].recordData[0].name}</span>
+												<div className='flex flex-col'><span className='text-sm'>改名为</span><img src={rightArrow} alt="---->" /></div>
+												<span className='w-40 text-xs text-left'></span>
+											</div>
+										</div>
+									</div>
+								</>}
+
+						</div>
 					</>
 				}
 				<div className={`w-screen  h-[calc(100vh-32px)] fixed z-10 top-8 left-0 ${!detailWinOpen && 'pointer-events-none'}`} onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
--- src/renderer/src/components/HomeStream/RecordsGroup.tsx
+++ src/renderer/src/components/HomeStream/RecordsGroup.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Record from "./Record"
+import { Checkbox } from "@mui/material";
 type recordData = {
+	key: number,
 	curDir: string,
 	displaySize: number,
 	handleDetailWinOpen: (e: HTMLImageElement, detailWinOpen: boolean) => void,
 	recordData: {
 		dateTitle: string,
 		recordData: Array<{ name: string, checked: boolean }>
-	}
+	},
+	setRecordData: (value: recordData["recordData"]) => void
+	// !来自eslint的规范
 }
-export default function RecordsGroup({ curDir, displaySize, handleDetailWinOpen, recordData }: recordData) {
+export default function RecordsGroup({ key, curDir, displaySize, handleDetailWinOpen, recordData, setRecordData }: recordData) {
 	return (
 		<>
 			<div className="w-full h-fit mb-3 px-4 py-2 rounded-2xl bg-gray-100">
-				<p className="w-1/3 border-b-2 border-gray-300 mb-2">{recordData.dateTitle}</p>
+				<div className="w-1/3 border-b-2 border-gray-300 mb-2 flex">
+					<Checkbox size="small" />
+					<p className="pt-2">{recordData.dateTitle}</p>
+				</div>
 				<div className="w-fit relative flex flex-wrap gap-3">
 					{recordData.recordData.map((item, index) => (
 						<Record key={index} curDir={curDir} displaySize={displaySize} handleDetailWinOpen={handleDetailWinOpen} RecordData={item} />
